cb050613695ee8601bc5338961b67e83984ef2d2
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-23 20:48:51 -0300

    Add project files.

---
+++ Aula10_Exercicio02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10_Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeano, idademes, idadedias;
+            Console.WriteLine("Informe sua idade em anos: ");
+            idadeano = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe sua idade em meses: ");
+            idademes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe sua idade em dias: ");
+            idadedias = int.Parse(Console.ReadLine());
+
+            int totaldias = (idadeano * 365) + (idademes * 30) + idadedias;
+            Console.WriteLine("Você tem " + totaldias + " dias vividos!");
+
+            Console.ReadKey();
+        }
+    }
+}
